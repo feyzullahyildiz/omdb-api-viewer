0e5dc3a2f34beed98685e1cca8bf4c81ecd38f96
A. Feyzullah YILDIZ <ahmet.feyzullah.yildiz@gmail.com>
2025-06-02 00:25:38 +0300

    api key from localstorage

--- src/api/index.ts
+++ src/api/index.ts
@@ -7,7 +7,24 @@ export const api = axios.create({
 api.interceptors.request.use((config) => {
     config.params = {
         ...config.params,
-        apikey: import.meta.env.VITE_OMDB_API_KEY || window.prompt("Enter your API key"),
+        apikey: getApiKey(),
       };
     return config
 })
+
+const LOCAL_STORAGE_KEY = "omdbApiKey";
+function getApiKey() {
+    const apiKey = import.meta.env.VITE_OMDB_API_KEY;
+    if (apiKey) {
+        return apiKey;
+    }
+    if (localStorage.getItem(LOCAL_STORAGE_KEY)) {
+        return localStorage.getItem(LOCAL_STORAGE_KEY);
+    }
+    const value = window.prompt("Enter your API key");
+    if (value) {
+        localStorage.setItem(LOCAL_STORAGE_KEY, value);
+        return value;
+    }
+    return null;
+}
